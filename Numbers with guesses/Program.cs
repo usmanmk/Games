f08d82f01f77a3f4e87b89f517a00182386d78aa
usman <usmanmk19@gmail.com>
2016-12-25 16:02:46 -0600

    hello

--- Numbers with guesses/Program.cs
+++ Numbers with guesses/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Numbers
+namespace Numbers_with_guesses
 {
     class Program
     {
@@ -25,44 +25,66 @@ namespace Numbers
 
                 int number2 = 0;
                 number2 = Convert.ToInt32(Console.ReadLine());
+
+
                 //options
                 Console.Write("Pick options 1.+ 2.- 3.* 4./ 5.% 6.~ :  ");
                 string option = Console.ReadLine();
-                
+                int result = 0;
                 if (option == "1" || option == "+")
                 {
-                    Console.WriteLine(number1 + number2);
+                    result = number1 + number2;
                 }
                 if (option == "2" || option == "-")
                 {
-                    Console.WriteLine(number1 - number2);
+                    result = number1 - number2;
                 }
                 if (option == "3" || option == "*")
                 {
-                    Console.WriteLine(number1 * number2);
+                    result = number1 * number2;
                 }
                 if (option == "4" || option == "/")
                 {
-                    Console.WriteLine(number1 / number2);
+                    result = number1 / number2;
                 }
                 if (option == "5" || option == "%")
                 {
-                    Console.WriteLine(number1 % number2);
+                    result = number1 % number2;
                 }
                 if (option == "6" || option == "~")
                 {
                     //   Environment.Exit(0);
                     break;
                 }
+                while (true)
 
+                {
+                    Console.Write("Enter guess ");
+                    int guess = Convert.ToInt32(Console.ReadLine());
+
+                    //equal 
+                    if (result == guess)
+                    {
+                        Console.WriteLine("Your guess is correct. Result is :  " + guess);
+                        break;
+                    }
+                    //less than
+                    if (guess < result)
+                    {
+                        Console.WriteLine("guess is LESS than result");
+                    }
+                    //greater than 
+                    if (guess > result)
+                    {
+                        Console.WriteLine("guess is MORE than result");
+                    }
+
+
+
+                }
 
 
             }
-
-
-
-
         }
-
     }
 }
